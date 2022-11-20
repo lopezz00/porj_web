9f39a8216ab7471a306df9653c8a8b85719c1d2e
FrancescNosas <francescnosas@gmail.com>
2022-11-20 22:44:44 +0100

    Cookies tests

--- js/cookies.js
+++ js/cookies.js
@@ -1,11 +1,16 @@
 
 function setCookie(name,value) {
-    document.cookie = name + "=" + value;
+    document.cookie = name + "=" + value + "; expires=Fri, 23 Dec 2022 12:00:00 UTC; path=/";
   }
 
-function submitForm() {
-    setCookie("name",document.getElementById("setname").value);
-    setCookie("password",document.getElementById("setpassw").value);
+function checkCookiesIndex(){
+  let email = getCookie("email");
+  if (email != ""){
+    alert("Welcome again");
+  }
 }
-
-document.getElementById("enviar").onclick = submitForm();
+function submitForm() {
+    setCookie("email",document.getElementById("email").value);
+    setCookie("password",document.getElementById("password").value);
+    document.location.href = '../index.html';
+}
