ec6d5cd340e04a2c90b6947e5e71216224df5a51
bastianmuan <118173732+bastianmuan@users.noreply.github.com>
2022-12-22 00:16:30 +0100

    Updated

--- admin-register-user.php
+++ admin-register-user.php
@@ -43,7 +43,6 @@ if (input.value != document.getElementById('password').value) {
   
     ?>
     
-<script src="./js/cookies.js">checkCookiesIndex();</script>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
